403be91d03b2f3af5e412783238096c580a425ce
Bekzodmirzaaliyev <bekzodmirzaaliyev27@gmail.com>
2025-09-09 16:54:22 +0500

    ASD

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <Link to="/" className='btn btn-primary btn-soft'>Login</Link>
+      {/* <Link to="/" className='btn btn-primary btn-soft'>Login</Link> */}
       {
         Outlet && <Outlet />
       }
---
+++ src/guard/VerifyGuard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+const VerifyGuard = ({ children }) => {
+    const user = useSelector(state => state?.auth?.user?.isVerified)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        console.log("SARDOR: ", user)
+        if(!user) {
+            navigate('/verify-account')
+        }
+    }, [user])
+    
+    return children
+}
+
+export default VerifyGuard
