4f976832a6ad02baba2898cad7af2b74996caeca
Bekzodmirzaaliyev <bekzodmirzaaliyev27@gmail.com>
2025-09-30 17:23:03 +0500

    ASD

--- src/pages/FilteredProducts.jsx
+++ src/pages/FilteredProducts.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import Container from '../components/shared/Container'
 
@@ -6,6 +6,22 @@ const FilteredProducts = () => {
     const { category } = useParams()
 
     console.log(category)
+
+    const requestProducts =  async () => {
+        try {
+            const request = await fetch(import.meta.env.VITE_API_URL + `/products/category/${category}`)
+            const response = await request.json()
+            console.log("azim dumba: ", response)
+        } catch(e) {
+            console.log("Server error:" , e)
+        } finally {
+
+        }
+    }
+
+    useEffect(() => {
+        requestProducts()
+    },[])
 
     return (
         <Container>
@@ -22,6 +38,7 @@ const FilteredProducts = () => {
                     </ul>
                 </div>
                 <div className='bg-green-400 flex-1'>
+                    <p>Category: {category}</p>
                     <div>
                         <select name="" id="" className="select"></select>
                     </div>
